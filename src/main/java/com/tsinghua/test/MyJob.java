ae07f21717d21a4f44f8db18c8925a5f276a2354
zwxu <xzw_6773@qq.com>
2016-04-07 20:13:26 +0800

    update

---
+++ src/main/java/com/tsinghua/test/MyJob.java
@@ -0,0 +1,21 @@
+/*
+ * @author sixi 
+ * @version 0.1 
+ * Company:Tsinghua
+ * Date:2012-09-15
+ * Description:����һ��ִ�е��ȵľ����࣬��Spring�����ļ���ָ�����ô����work����
+ * */
+package com.tsinghua.test;
+
+import java.util.Date;
+
+
+public class MyJob {
+
+	
+	public void work()
+	{
+		 System.out.println("当前时间:"+new Date().toString());  
+	}
+}//End of MyJob
+
